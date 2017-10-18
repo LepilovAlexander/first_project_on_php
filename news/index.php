4cc7e4db51421bd13a13edc5ec7f6a85413489b0
lepilov_alexander <temnoles666@gmail.com>
2017-10-18 22:56:31 +0300

    compl. menu change active: news and production

--- news/index.php
+++ news/index.php
@@ -3,5 +3,6 @@
 	$title = "Новости от watch.loc.ua";
 	$descr = "Здесь ві можете узнать последние новости о.....";
 	$contentView = "newsView.php";
+	$active = "news";
     require_once "../views/mainTemplate.php";
 ?>
--- production/index.php
+++ production/index.php
@@ -3,5 +3,6 @@
 	$title = "Продукция сайта watch.loc.ua";
 	$descr = "Продукция интернет магазина....";
 	$contentView = "productionView.php";
+	$active = "production";
     require_once "../views/mainTemplate.php";
 ?>
