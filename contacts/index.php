906e437c95e245bfd3ae7a3221dc968a467320b2
lepilov_alexander <temnoles666@gmail.com>
2017-10-19 11:11:27 +0300

    add contacts.php, contacts folder and contactsVieaw.php

---
+++ contacts/index.php
@@ -0,0 +1,8 @@
+<? 
+	// Контакты
+	$title = "Контактная информация watch.loc.ua";
+	$descr = "Контакты, телефон эмейл, имейл, email";
+	$contentView = "contactsView.php";
+	$active = "contacts";
+    require_once "../views/mainTemplate.php";
+?>
---
+++ views/contactsView.php
@@ -0,0 +1,10 @@
+<div class="row">
+<ul>
+	<li>Lorem ipsum.</li>
+	<li>Quaerat, aperiam!</li>
+	<li>Similique, asperiores.</li>
+	<li>Ex, reprehenderit!</li>
+	<li>Sunt, libero.</li>
+	<li>Praesentium, eius.</li>
+</ul>
+</div>
